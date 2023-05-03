841e095a86611bf64fd522cd99234fea0c699c5a
tarsil <tiago.arasilva@gmail.com>
2023-05-03 18:51:40 +0100

    ➕ Custom fields for dates, booleans and floats

--- edgy/core/db/fields.py
+++ edgy/core/db/fields.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import Any, Optional, Sequence
 
 from edgy.core.db.base import BaseField
@@ -10,7 +11,7 @@ class FieldFactory:
     """The base for all model fields to be used with EdgeDB"""
 
     _bases = (BaseField,)
-    _is_property: bool = False
+    _property: bool = False
     _is_link: bool = False
     _type: Any = None
 
@@ -27,7 +28,7 @@ class FieldFactory:
         choices: Sequence = set(kwargs.pop("choices", []))
 
         field_type = cls._type
-        is_property = cls._is_property
+        is_property = cls._property
         is_link = cls._is_link
 
         namespace = dict(
@@ -59,14 +60,14 @@ class FieldFactory:
     @classmethod
     def get_column_type(cls, **kwargs) -> Any:
         """Returns the propery column type for the field"""
-        return None
+        return cls._type
 
 
 class StringField(FieldFactory, str):
     """String field representation that constructs the Field class and populates the values"""
 
     _type = str
-    _is_property: bool = True
+    _property: bool = True
 
     def __new__(  # type: ignore # noqa CFQ002
         cls,
@@ -87,3 +88,139 @@ class StringField(FieldFactory, str):
         max_length = kwargs.get("max_length", 0)
         if max_length <= 0:
             raise FieldDefinitionError(detail=f"'max_length' is required for {cls.__name__}")
+
+
+class TextField(FieldFactory, str):
+    """String representation of a text field which means no max_length required"""
+
+    _type = str
+    _property: bool = True
+
+    def __new__(cls, **kwargs: Any) -> BaseField:  # type: ignore
+        kwargs = {
+            **kwargs,
+            **{key: value for key, value in locals().items() if key not in CLASS_DEFAULTS},
+        }
+        return super().__new__(cls, **kwargs)
+
+
+class FloatField(FieldFactory, float):
+    """Representation of a int32 and int64"""
+
+    _type = float
+    _property: bool = True
+
+    def __new__(
+        cls,
+        *,
+        mininum: Optional[float] = None,
+        maximun: Optional[float] = None,
+        multiple_of: Optional[int] = None,
+        **kwargs: Any,
+    ) -> BaseField:
+        kwargs = {
+            **kwargs,
+            **{key: value for key, value in locals().items() if key not in CLASS_DEFAULTS},
+        }
+        return super().__new__(cls, **kwargs)
+
+
+class BooleanField(FieldFactory, int):
+    """Representation of a boolean"""
+
+    _type = bool
+    _property: bool = True
+
+    def __new__(
+        cls,
+        *,
+        default: Optional[bool] = False,
+        **kwargs: Any,
+    ) -> BaseField:
+        kwargs = {
+            **kwargs,
+            **{key: value for key, value in locals().items() if key not in CLASS_DEFAULTS},
+        }
+        return super().__new__(cls, **kwargs)
+
+
+class AutoNowMixin(FieldFactory):
+    def __new__(  # type: ignore # noqa CFQ002
+        cls,
+        *,
+        auto_now: Optional[bool] = False,
+        auto_now_add: Optional[bool] = False,
+        **kwargs: Any,
+    ) -> BaseField:  # type: ignore
+        if auto_now_add and auto_now:
+            raise FieldDefinitionError("'auto_now' and 'auto_now_add' cannot be both True")
+        if auto_now_add or auto_now:
+            kwargs["read_only"] = True
+
+        kwargs = {
+            **kwargs,
+            **{k: v for k, v in locals().items() if k not in CLASS_DEFAULTS},
+        }
+        return super().__new__(cls, **kwargs)
+
+
+class DateTimeField(AutoNowMixin, datetime.datetime):
+    """Representation of a datetime field"""
+
+    _type = datetime.datetime
+    _property: bool = True
+
+    def __new__(  # type: ignore # noqa CFQ002
+        cls,
+        *,
+        auto_now: Optional[bool] = False,
+        auto_now_add: Optional[bool] = False,
+        **kwargs: Any,
+    ) -> BaseField:  # type: ignore
+        if auto_now_add or auto_now:
+            kwargs["default"] = datetime.datetime.now
+
+        kwargs = {
+            **kwargs,
+            **{k: v for k, v in locals().items() if k not in CLASS_DEFAULTS},
+        }
+        return super().__new__(cls, **kwargs)
+
+
+class DateField(AutoNowMixin, datetime.date):
+    """Representation of a date field"""
+
+    _type = datetime.date
+    _property: bool = True
+
+    def __new__(  # type: ignore # noqa CFQ002
+        cls,
+        *,
+        auto_now: Optional[bool] = False,
+        auto_now_add: Optional[bool] = False,
+        **kwargs: Any,
+    ) -> BaseField:  # type: ignore
+        if auto_now_add or auto_now:
+            kwargs["default"] = datetime.datetime.today
+
+        kwargs = {
+            **kwargs,
+            **{k: v for k, v in locals().items() if k not in CLASS_DEFAULTS},
+        }
+        return super().__new__(cls, **kwargs)
+
+
+class TimeField(FieldFactory, datetime.time):
+    """Representation of a time field"""
+
+    _type = datetime.time
+    _property: bool = True
+
+    def __new__(  # type: ignore # noqa CFQ002
+        cls, **kwargs: Any
+    ) -> BaseField:  # type: ignore
+        kwargs = {
+            **kwargs,
+            **{k: v for k, v in locals().items() if k not in CLASS_DEFAULTS},
+        }
+        return super().__new__(cls, **kwargs)
--- tests/core/test_fields.py
+++ tests/core/test_fields.py
@@ -1,19 +1,103 @@
+import datetime
+
 import pytest
 
 from edgy.core.db.base import BaseField
-from edgy.core.db.fields import StringField
+from edgy.core.db.fields import (
+    BooleanField,
+    DateField,
+    DateTimeField,
+    FloatField,
+    StringField,
+    TextField,
+    TimeField,
+)
 from edgy.exceptions import FieldDefinitionError
 
 
-def test_can_create_string_field():
-    string = StringField(min_length=5, max_length=10, null=True)
+def test_column_type():
+    assert StringField.get_column_type() == str
+    assert TextField.get_column_type() == str
+    assert FloatField.get_column_type() == float
+    assert BooleanField.get_column_type() == bool
+    assert DateTimeField.get_column_type() == datetime.datetime
+    assert DateField.get_column_type() == datetime.date
+    assert TimeField.get_column_type() == datetime.time
 
-    assert isinstance(string, BaseField)
-    assert string.min_length == 5
-    assert string.max_length == 10
-    assert string.null is True
+
+def test_can_create_string_field():
+    field = StringField(min_length=5, max_length=10, null=True)
+
+    assert isinstance(field, BaseField)
+    assert field.min_length == 5
+    assert field.max_length == 10
+    assert field.null is True
 
 
 def test_raises_field_definition_error_on_string_creation():
     with pytest.raises(FieldDefinitionError):
         StringField(min_length=10, null=False)
+
+
+def test_can_create_text_field():
+    field = TextField(min_length=5, null=True)
+
+    assert isinstance(field, BaseField)
+    assert field.min_length == 5
+    assert field.max_length is None
+    assert field.null is True
+
+
+def test_can_create_float_field():
+    field = FloatField(minimum=5, maximum=10, null=True)
+
+    assert isinstance(field, BaseField)
+    assert field.minimum == 5
+    assert field.maximum == 10
+    assert field.null is True
+
+
+def test_can_create_boolean_field():
+    field = BooleanField(default=False)
+
+    assert isinstance(field, BaseField)
+    assert field.default is False
+
+    field = BooleanField(default=True)
+    assert field.default is True
+
+    field = BooleanField()
+    assert field.default is False
+
+
+def test_can_create_datetime_field():
+    field = DateTimeField(auto_now=True)
+
+    assert isinstance(field, BaseField)
+    assert field.default == datetime.datetime.now
+    assert field.read_only is True
+
+
+def test_raises_field_definition_error_on_datetime_creation():
+    with pytest.raises(FieldDefinitionError):
+        DateTimeField(auto_now=True, auto_now_add=True)
+
+
+def test_can_create_date_field():
+    field = DateField(auto_now=True)
+
+    assert isinstance(field, BaseField)
+    assert field.default == datetime.datetime.today
+    assert field.read_only is True
+
+
+def test_raises_field_definition_error_on_date_creation():
+    with pytest.raises(FieldDefinitionError):
+        DateTimeField(auto_now=True, auto_now_add=True)
+
+
+def test_can_create_time_field():
+    field = TimeField(auto_now=True)
+
+    assert isinstance(field, BaseField)
+    assert field.read_only is False
