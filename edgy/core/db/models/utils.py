67d20b50a740696ba8844395027924a978bb04ed
tarsil <tiago.arasilva@gmail.com>
2023-08-11 11:50:46 +0100

    Add get_model function to codebase

---
+++ edgy/core/db/models/utils.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING, Type, cast
+
+from edgy.core.connection.registry import Registry
+
+if TYPE_CHECKING:
+    from edgy.core.db.models.model import Model
+
+
+def get_model(registry: Registry, model_name: str) -> Type["Model"]:
+    """
+    Return the model with capitalize model_name.
+
+    Raise lookup error if no model is found.
+    """
+    try:
+        return cast("Type[Model]", registry.models[model_name.capitalize()])
+    except KeyError:
+        raise LookupError(f"Registry doesn't have a {model_name.capitalize()} model.") from None
