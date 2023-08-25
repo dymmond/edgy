b02e5936e77e2ff9dad43cf7f3eac4f530650550
Tiago Silva <tiago.arasilva@gmail.com>
2023-08-25 17:57:38 +0100

    Update base queryset and remove model references for signal sending

--- edgy/core/db/querysets/base.py
+++ edgy/core/db/querysets/base.py
@@ -838,15 +838,8 @@ class QuerySet(BaseQuerySet, QuerySetProtocol):
         queryset.set_query_expression(expression)
         await queryset.database.execute_many(str(expression), query_list)
 
-        # Broadcast the signal
-        await self.model_class.meta.signals.post_bulk_update.send(
-            sender=self.model_class, instances=query_list
-        )
-
     async def delete(self) -> None:
         queryset: "QuerySet" = self.clone()
-
-        await self.model_class.signals.pre_delete.send(sender=self.__class__, instance=self)
 
         expression = queryset.table.delete()
         for filter_clause in queryset.filter_clauses:
@@ -854,8 +847,6 @@ class QuerySet(BaseQuerySet, QuerySetProtocol):
 
         queryset.set_query_expression(expression)
         await queryset.database.execute(expression)
-
-        await self.model_class.signals.post_delete.send(sender=self.__class__, instance=self)
 
     async def update(self, **kwargs: Any) -> None:
         """
@@ -868,11 +859,6 @@ class QuerySet(BaseQuerySet, QuerySetProtocol):
         )
         kwargs = queryset.update_auto_now_fields(extracted_fields, queryset.model_class.fields)
 
-        # Broadcast the initial update details
-        await self.model_class.signals.pre_update.send(
-            sender=self.__class__, instance=self, kwargs=kwargs
-        )
-
         expression = queryset.table.update().values(**kwargs)
 
         for filter_clause in queryset.filter_clauses:
@@ -880,9 +866,6 @@ class QuerySet(BaseQuerySet, QuerySetProtocol):
 
         queryset.set_query_expression(expression)
         await queryset.database.execute(expression)
-
-        # Broadcast the update executed
-        await self.model_class.signals.post_update.send(sender=self.__class__, instance=self)
 
     async def get_or_create(
         self, defaults: Dict[str, Any], **kwargs: Any
