a6d4f1532f07f3be565b07371eff54c56f922d30
tarsil <tiago.arasilva@gmail.com>
2023-11-03 16:21:25 +0000

    Add get_foreign_keys functionality

--- edgy/cli/operations/inspectdb.py
+++ edgy/cli/operations/inspectdb.py
@@ -1,10 +1,12 @@
-from typing import Union
+from typing import Any, Dict, List, Union
 
 import click
 import sqlalchemy
 from loguru import logger
 from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.sql import sqltypes
 
+import edgy
 from edgy import Database, Registry
 from edgy.cli.env import MigrationEnv
 from edgy.cli.exceptions import MissingParameterException
@@ -12,6 +14,25 @@ from edgy.core.sync import execsync
 from edgy.core.terminal import Print
 
 printer = Print()
+
+
+SQL_MAPPING_TYPES = {
+    sqltypes.BIGINT: edgy.BigIntegerField,
+    sqltypes.INTEGER: edgy.IntegerField,
+    sqltypes.JSON: edgy.JSONField,
+    sqltypes.DATE: edgy.DateField,
+    sqltypes.VARCHAR: edgy.CharField,
+    sqltypes.BINARY: edgy.BinaryField,
+    sqltypes.BOOLEAN: edgy.BooleanField,
+    sqltypes.Enum: edgy.ChoiceField,
+    sqltypes.DATETIME: edgy.DateTimeField,
+    sqltypes.DECIMAL: edgy.DecimalField,
+    sqltypes.FLOAT: edgy.FloatField,
+    sqltypes.SMALLINT: edgy.SmallIntegerField,
+    sqltypes.TEXT: edgy.TextField,
+    sqltypes.TIME: edgy.TimeField,
+    sqltypes.UUID: edgy.UUIDField,
+}
 
 
 @click.option(
@@ -92,6 +113,53 @@ def inspect_db(
         sqlalchemy.MetaData(schema=schema) if schema is not None else sqlalchemy.MetaData()
     )
     metadata = execsync(reflect)(engine=engine, metadata=metadata)
+
+    # Generate the tables
+    generate_tables(metadata, registry)
+
+
+def generate_tables(metadata: sqlalchemy.MetaData, registry: Registry) -> Registry:
+    """
+    Generates the tables from the reflection and maps them into the
+    `reflected` dictionary of the `Registry`.
+    """
+    tables_dict = dict(metadata.tables.items())
+    tables = []
+
+    for key, table in tables_dict.items():
+        table_details: Dict[str, Any] = {}
+        table_details["tablename"] = key
+        table_details["class_name"] = key.replace("_", "").capitalize()
+        table_details["table"] = table
+
+        # Get the details of the foreign key
+        table_details["foreign_keys"] = get_foreign_keys(table)
+
+        # Get the details of the indexes
+        table_details["indexes"] = table.indexes
+        tables.append(table_details)
+
+    return registry
+
+
+def get_foreign_keys(table: sqlalchemy.Table) -> List[Dict[str, Any]]:
+    """
+    Extracts all the information needed of the foreign keys.
+    """
+    details: List[Dict[str, Any]] = []
+
+    for foreign_key in table.foreign_keys:
+        fk: Dict[str, Any] = {}
+        fk["column"] = foreign_key.column
+        fk["column_name"] = foreign_key.column.name
+        fk["tablename"] = foreign_key.column.table.name
+        fk["class_name"] = foreign_key.column.table.name.replace("_", "").capitalize()
+        fk["on_delete"] = foreign_key.ondelete
+        fk["on_update"] = foreign_key.onupdate
+        fk["null"] = foreign_key.column.nullable
+        details.append(fk)
+
+    return details
 
 
 async def reflect(
