7a009acef1b70c425a65b2939f418857a4b43bd9
alex <devkral@web.de>
2024-08-13 13:15:16 +0200

    remove dependency on nest_asyncio

--- edgy/core/utils/sync.py
+++ edgy/core/utils/sync.py
@@ -1,13 +1,22 @@
 import asyncio
 from typing import Any, Awaitable
 
-import nest_asyncio
 
-nest_asyncio.apply()
+async def _await_helper(awaitable: Awaitable) -> Any:
+    return await awaitable
 
 
 def run_sync(awaitable: Awaitable) -> Any:
     """
     Runs the queries in sync mode
     """
-    return asyncio.run(awaitable)
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = None
+    if loop is None:
+        return asyncio.run(awaitable)
+    else:
+        task = loop.create_task(_await_helper(awaitable))
+        loop.run_until_complete(task)
+        return task.result()
