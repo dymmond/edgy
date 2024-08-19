6bcf37cd91ddeae366983e38d6940d44b17f54ea
Alexander <devkral@web.de>
2024-08-19 14:44:51 +0200

    inspect & run_sync: try harder to stay in the same asyncio loop. (#163)
    Changes:
    * run inspect sync in one call
    * run_sync run in same loop instead of creating a nested one

--- edgy/core/utils/sync.py
+++ edgy/core/utils/sync.py
@@ -10,4 +10,12 @@ def run_sync(awaitable: Awaitable) -> Any:
     """
     Runs the queries in sync mode
     """
-    return asyncio.run(awaitable)
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = None
+    if loop is None:
+        return asyncio.run(awaitable)
+    else:
+        # nested loop run, only possible with nest_asyncio
+        return loop.run_until_complete(awaitable)
