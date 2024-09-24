c4b759bb5ea05ae0ff5dd2155e289db38699be94
alex <devkral@web.de>
2024-09-24 13:35:21 +0200

    simplify multi tenancy metaclasses code

--- edgy/contrib/multi_tenancy/metaclasses.py
+++ edgy/contrib/multi_tenancy/metaclasses.py
@@ -3,9 +3,7 @@ from typing import Any, Optional, Tuple, Type, cast
 from edgy.core.db.models.metaclasses import (
     BaseModelMeta,
     MetaInfo,
-    get_model_registry,
 )
-from edgy.exceptions import ImproperlyConfigured
 
 
 def _check_model_inherited_tenancy(bases: Tuple[Type, ...]) -> bool:
@@ -21,9 +19,10 @@ def _check_model_inherited_tenancy(bases: Tuple[Type, ...]) -> bool:
 
 class TenantMeta(MetaInfo):
     __slots__ = ("is_tenant", "register_default")
+    register_default: bool
 
     def __init__(self, meta: Any = None, **kwargs: Any) -> None:
-        self.is_tenant: bool = getattr(meta, "is_tenant", False)
+        self.is_tenant = getattr(meta, "is_tenant", None)
         # register in models too, so there is a default
         # otherwise they only exist as tenant models
         self.register_default: bool = getattr(meta, "register_default", True)
@@ -44,37 +43,22 @@ class BaseTenantMeta(BaseModelMeta):
     """
 
     def __new__(cls, name: str, bases: Tuple[Type, ...], attrs: Any, **kwargs: Any) -> Any:
-        meta_class: object = attrs.get("Meta", type("Meta", (), {}))
         new_model = super().__new__(cls, name, bases, attrs, meta_info_class=TenantMeta, **kwargs)
-        meta = new_model.meta
+        if new_model.meta.is_tenant is None:
+            new_model.meta.is_tenant = _check_model_inherited_tenancy(bases)
 
-        if hasattr(meta_class, "is_tenant"):
-            meta.is_tenant = cast(bool, meta_class.is_tenant)
-        else:
-            meta.is_tenant = _check_model_inherited_tenancy(bases)
-
-        if hasattr(meta_class, "register_default"):
-            meta.register_default = cast(bool, meta_class.register_default)
-
-        # Handle the registry of models
-        if getattr(meta, "registry", None) is None:
-            if getattr(new_model, "__db_model__", False):
-                meta.registry = get_model_registry(bases)
-            else:
-                return new_model
-        registry = meta.registry
-        if registry is None:
-            raise ImproperlyConfigured(
-                "Registry for the table not found in the Meta class or any of the superclasses. You must set the registry in the Meta."
-            )
-
-        if registry and meta.is_tenant and not meta.abstract and not new_model.__is_proxy_model__:
+        if (
+            new_model.meta.registry
+            and new_model.meta.is_tenant
+            and not new_model.meta.abstract
+            and not new_model.__is_proxy_model__
+        ):
             assert (
                 new_model.__reflected__ is False
             ), "Reflected models are not compatible with multi_tenancy"
 
-            if not meta.register_default:
+            if not new_model.meta.register_default:
                 # remove from models
-                registry.models.pop(new_model.__name__, None)
-            registry.tenant_models[new_model.__name__] = new_model
+                new_model.meta.registry.models.pop(new_model.__name__, None)
+            new_model.meta.registry.tenant_models[new_model.__name__] = new_model
         return new_model
