5e5a7e879d0a16bc9a7ee4ca519312375d7da535
alex <devkral@web.de>
2024-10-14 19:12:59 +0200

    Changes:
    - update docs
    - bump version

--- docs_src/queries/clauses/style/or.py
+++ docs_src/queries/clauses/style/or.py
@@ -5,5 +5,5 @@ import edgy
 await User.query.create(name="Adam", email="adam@edgy.dev")
 await User.query.create(name="Eve", email="eve@edgy.dev")
 
-# Query using the or_
+# Query using the global or_ with multiple ANDed field queries
 await User.query.or_(name="Adam", email="adam@edgy.dev")
--- docs_src/queries/clauses/style/or_two.py
+++ docs_src/queries/clauses/style/or_two.py
@@ -4,7 +4,10 @@ await User.query.create(name="Adam", email="adam@edgy.dev")
 await User.query.create(name="Eve", email="eve@edgy.dev")
 
 # Query using the multiple or_
+await User.query.or_({"email__icontains": "edgy"}, {"name__icontains": "a"})
+
+# QUery using the global or
 await User.query.or_(email__icontains="edgy").or_(name__icontains="a")
 
-# Query using the or_ with multiple fields
+# Query using the or_ with multiple ANDed field queries
 await User.query.or_(email__icontains="edgy", name__icontains="a")
