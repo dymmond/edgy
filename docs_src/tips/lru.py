d1021f76b8fd28b0b0dca17c69da71a3bdc9025e
Alexander <devkral@web.de>
2024-12-28 22:30:06 +0100

    update tips and tricks section (#255)
    Changes:
    
    - show how to rewire edgy settings
    - show modern project layout
    - signature formatting with ruff

--- docs_src/tips/lru.py
+++ docs_src/tips/lru.py
@@ -1,8 +1,23 @@
+import sys
+import os
 from functools import lru_cache
 
-from esmerald.conf import settings
+
+# we need this also here because we access settings and must be sure the import path is importable
+def build_path():
+    """
+    Builds the path of the project and project root.
+    """
+    SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+    if SITE_ROOT not in sys.path:
+        sys.path.append(SITE_ROOT)
+        # in case of apps
+        sys.path.append(os.path.join(SITE_ROOT, "apps"))
 
 
 @lru_cache()
 def get_db_connection():
+    from esmerald.conf import settings
+
     return settings.db_connection
