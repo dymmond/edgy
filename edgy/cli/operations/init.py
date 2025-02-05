65f3f3c2814c16afe0d1913bf461e2db7e161cc4
tarsil <tiago.arasilva@gmail.com>
2025-02-05 19:06:42 +0100

    Rollback helper

--- edgy/cli/operations/init.py
+++ edgy/cli/operations/init.py
@@ -15,20 +15,5 @@ from edgy.cli.decorators import add_migration_directory_option
 )
 @click.command(name="init")
 def init(template: str, package: bool) -> None:
-    """
-    Repository template to use (default is "default")'
-
-    The options are:
-
-    - default: The default template
-
-    - plain: A plain template with no environment or version files
-
-    - sequencial: A template with a sequencial versioning scheme. E.g. 0001_initial, 0002_second,
-    etc.
-
-    Example:
-
-        edgy init -t sequencial
-    """
+    """Creates a new migration repository."""
     _init(template, package)
