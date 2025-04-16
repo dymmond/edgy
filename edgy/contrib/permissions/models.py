5d419411becf5055ff94be96c4380792f27f3840
alex <devkral@web.de>
2025-04-16 07:48:32 +0200

    fix typing

--- edgy/contrib/permissions/models.py
+++ edgy/contrib/permissions/models.py
@@ -8,7 +8,7 @@ from .managers import PermissionManager
 class BasePermission(edgy.Model):
     users_field_group: ClassVar[str] = "users"
     name: str = edgy.fields.CharField(max_length=100, null=False)
-    description: Optional[str] = edgy.fields.ComputedField(
+    description: Optional[str] = edgy.fields.ComputedField(  # type: ignore
         getter="get_description",
         setter="set_description",
         fallback_getter=lambda field, instance, owner: instance.name,
