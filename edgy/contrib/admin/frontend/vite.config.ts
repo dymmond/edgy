9c636bfee05eaa459c6ade630c68aed177550eea
tarsil <tiago.arasilva@gmail.com>
2025-05-03 16:02:18 +0200

    Build command added

--- edgy/contrib/admin/frontend/vite.config.ts
+++ edgy/contrib/admin/frontend/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../static', // or 'dist'
+  },
+  server: {
+    port: 5173
+  }
 })
