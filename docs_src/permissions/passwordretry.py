4c58a851c54b4efbac86a7461d046bffa1ee9ce4
alex <devkral@web.de>
2025-09-21 01:53:46 +0200

    improve example

--- docs_src/permissions/passwordretry.py
+++ docs_src/permissions/passwordretry.py
@@ -1,8 +1,9 @@
 import edgy
 from contextlib import suppress
+from typing import Self, cast
 
 from passlib.context import CryptContext
-from pydantic import ValidationError
+from pydantic import ValidationError, model_validator
 
 pwd_context = CryptContext(
     # Replace this list with the hash(es) you wish to support.
@@ -11,47 +12,56 @@ pwd_context = CryptContext(
 )
 
 
-class User(edgy.Model):
-    name: str = edgy.CharField(max_length=255)
-    # derive_fn implies the original password is saved in <field>_original
-    pw: str = edgy.PasswordField(null=False, derive_fn=pwd_context.hash)
-    ...
-
-    @model_validator(mode="after")
-    def other_validation_check(self) -> str:
-        if getattr(self, "pw_original", None) is not None and self.pw_original == "":
-            raise ValueError("must not be empty")
-        return self
-
-
 def validate_password_strength(password: str) -> None:
     if len(password) < 10:
         raise ValueError()
 
 
-async def create_user(pw1, pw2):
-    user = User(name="edgy", pw=(pw1, pw2))
+class User(edgy.Model):
+    name: str = edgy.CharField(max_length=255)
+    # derive_fn implies the original password is saved in <field>_original
+    password: str = edgy.PasswordField(null=False, derive_fn=pwd_context.hash)
+    ...
+
+    @model_validator(mode="after")
+    def other_validation_check(self) -> Self:
+        if getattr(self, "password_original", None) is None:
+            return self
+        if self.password_original == "":
+            raise ValueError("must not be empty")
+        # for demonstration purposes we don't do it in model_validator
+        # validate_password_strength(self.password_original)
+        return self
+
+
+async def create_user(name: str, pw1: str, pw2: str) -> tuple[bool, User]:
+    user = User(name=name, password=(pw1, pw2))
 
     try:
         # operate on the non-hashed original
-        validate_password_strength(user.pw_original)
-        return True, await model.save()
+        # for demonstration purposes we don't do it in model_validator
+        validate_password_strength(user.password_original)
+        return True, cast(User, await user.save())
     except Exception:
         # ooops something went wrong, we want to show the user his password again, unhashed
-        model.__dict__["pw"] = model.pw_original
-        return False, model
+        user.__dict__["password"] = user.password_original
+        return False, user
 
 
 with suppress(ValidationError):
     # model validator fails
-    edgy.run_sync(create_user("", ""))
+    edgy.run_sync(create_user("edgy", "", ""))
 
 # pw strength fails
-success, halfinitialized_model = edgy.run_sync(create_user("foobar", "foobar"))
+success, halfinitialized_model = edgy.run_sync(create_user("edgy", "foobar", "foobar"))
 
 # works
 user = edgy.run_sync(
-    create_user(halfinitialized_model.pw + "12345678", halfinitialized_model.pw + "12345678")
+    create_user(
+        "edgy",
+        halfinitialized_model.password + "12345678",
+        halfinitialized_model.password + "12345678",
+    )
 )
 
 # comparing
