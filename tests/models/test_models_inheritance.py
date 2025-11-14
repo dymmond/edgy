933ecb350c245df396db041487fb312e43bc2eed
alex <devkral@web.de>
2025-11-14 21:09:13 +0100

    add testcase for model inheritance

---
+++ tests/models/test_models_inheritance.py
@@ -0,0 +1,53 @@
+from typing import ClassVar
+
+import pytest
+
+import edgy
+from tests.settings import DATABASE_URL
+
+pytestmark = pytest.mark.anyio
+
+# here we don't initialize any database
+
+
+def test_second_manager():
+    models = edgy.Registry(DATABASE_URL)
+
+    class DjangoBase(edgy.StrictModel):
+        class Meta:
+            abstract = True
+
+        objects: ClassVar[edgy.Manager] = edgy.RedirectManager(redirect_name="query")
+
+    class Cat(DjangoBase):
+        class Meta:
+            registry = models
+
+        objects: ClassVar[edgy.Manager] = edgy.RedirectManager(redirect_name="query")
+
+    class Cat2(Cat, DjangoBase):
+        class Meta:
+            registry = models
+
+        objects2: ClassVar[edgy.Manager] = edgy.RedirectManager(redirect_name="query")
+
+
+def test_abstract_registry():
+    models = edgy.Registry(DATABASE_URL)
+
+    class DjangoBase(edgy.StrictModel):
+        class Meta:
+            abstract = True
+            registry = models
+
+        objects: ClassVar[edgy.Manager] = edgy.RedirectManager(redirect_name="query")
+
+    class Cat(DjangoBase):
+        objects: ClassVar[edgy.Manager] = edgy.RedirectManager(redirect_name="query")
+
+    class Cat2(Cat, DjangoBase):
+        objects2: ClassVar[edgy.Manager] = edgy.RedirectManager(redirect_name="query")
+
+    assert "DjangoBase" not in models.models
+    assert "Cat" in models.models
+    assert "Cat2" in models.models
