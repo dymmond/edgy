9c13e5db2802197323e57fd6840abfc15e5987de
tarsil <tiago.arasilva@gmail.com>
2025-11-20 01:29:59 +0100

    Add missing settings into the __init__

--- edgy/__init__.py
+++ edgy/__init__.py
@@ -7,6 +7,7 @@ from ._monkay import Instance, create_monkay
 from .core.utils.sync import run_sync
 
 if TYPE_CHECKING:
+    from .conf import settings
     from .conf.global_settings import EdgySettings
     from .core import files, marshalls
     from .core.connection import Database, DatabaseURL, Registry
