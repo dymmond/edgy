82b1e4876c5bc76a3a7d7ae18c0918e875d4809e
Tiago Silva <tiago.arasilva@gmail.com>
2026-06-16 10:34:20 +0100

    chore(tests): remove unsused test

--- tests/test_testclient_aliases.py
+++
@@ -1,6 +0,0 @@
-from edgy.testclient import DatabaseTestClient, EdgyTestClient, EggyTestClient
-
-
-def test_edgy_testclient_aliases() -> None:
-    assert issubclass(EdgyTestClient, DatabaseTestClient)
-    assert EggyTestClient is EdgyTestClient
