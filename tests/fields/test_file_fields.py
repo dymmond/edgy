a235dc37673739fd0b3c8db81d985d8a370407fb
alex <devkral@web.de>
2026-09-23 09:22:33 +0200

    fix file test

--- tests/fields/test_file_fields.py
+++ tests/fields/test_file_fields.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime, timezone
 from typing import Any
 from unittest.mock import call
 from uuid import uuid4
@@ -121,10 +122,16 @@ async def test_save_file_create(create_test_database):
     name = model.file_field.name
     stats = os.stat(model.file_field.storage.path(name))
     # the floats must be the same, so no need to add an epsilon
-    assert model.file_field.storage.get_accessed_time(name) == stats.st_atime
-    assert model.file_field.storage.get_modified_time(name) == stats.st_mtime
+    assert model.file_field.storage.get_accessed_time(name) == datetime.fromtimestamp(
+        stats.st_atime, tz=timezone.utc
+    )
+    assert model.file_field.storage.get_modified_time(name) == datetime.fromtimestamp(
+        stats.st_mtime, tz=timezone.utc
+    )
     if hasattr(stats, "st_birthtime"):
-        assert model.file_field.storage.get_created_time(name) == stats.st_birthtime
+        assert model.file_field.storage.get_created_time(name) == datetime.fromtimestamp(
+            stats.st_birthtime, tz=timezone.utc
+        )
     assert os.path.exists(path)
     assert model.file_field.storage.exists(model.file_field.name)
     model.file_field.delete()
